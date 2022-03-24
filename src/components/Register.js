67305af647aa1466b3506a165e25e161069bd0a2
timothyrusso <russotimothy@live.it>
2022-03-24 08:32:26 +0100

    feat: input controlled configuration on register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import SignForm from "./SignForm";
 import FormRedirect from "./FormRedirect";
+import { useNavigate } from 'react-router-dom';
 
 function Register() {
+
+    const [inputs, setInputs] = useState({
+        email: '',
+        password: ''
+    });
+    const [message, setMessage] = useState("");
+
+    const history = useNavigate();
+
     return (
         <>
             <SignForm title="Sign up" />
