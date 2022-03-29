284bddbd59f845d19353bbb89099beac32c70945
timothyrusso <russotimothy@live.it>
2022-03-29 06:40:27 +0200

    feat: basic styling for infoTooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Popup from "./Popup";
+import successImage from "../images/success.svg"
+import failedImage from "../images/failed.svg"
 
 function InfoTooltip({ isOpen, onClose, status }) {
 
     return (
         <Popup isOpen={isOpen} onClose={onClose}>
-            <h2 className="popup__title" >{status === "success" ? "ottimo" : "male"}</h2>
+            <h2 className="popup__title" >{
+                status === "success" ?
+                    <div>
+                        <img src={successImage} alt="Success icon"></img>
+                        <p>Success! You have now been registered.</p>
+                    </div>
+                    :
+                    <div>
+                        <img src={failedImage} alt="Failed icon"></img>
+                        <p>Oops, something went wrong! Please try again.</p>
+                    </div>
+            }</h2>
         </Popup>
     )
 }
