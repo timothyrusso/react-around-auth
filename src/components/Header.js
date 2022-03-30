711b4e38a833991bbfea67f4766f173356b95622
Timothy Russo <russo.t@cisalfasport.it>
2022-03-30 16:14:50 +0200

    feat: mobile navbar optimization

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,21 +30,21 @@ function Header({ loggedIn, handleLogout, userEmail }) {
         }
     }
 
-    console.log(toggleMenu);
-
     return (
-        <header className="header">
-            {toggleMenu &&
+        <>
+            {loggedIn && toggleMenu &&
                 <div className="mobile-navbar"><p className="header__email">{loggedIn && userEmail}</p>
                     <Link to={pathDefinition()} className={`header__link ${loggedIn ? "header__link_type_logout" : ""}`} onClick={loggedIn && handleLogout}>{linkText}</Link>
                 </div>}
-            <img src={logo} alt="Logo representing the Around the US project" className="logo" />
-            <div className="header__wrapper">
-                <p className="header__email">{loggedIn && userEmail}</p>
-                <Link to={pathDefinition()} className={`header__link ${loggedIn ? "header__link_type_logout" : ""}`} onClick={loggedIn && handleLogout}>{linkText}</Link>
-            </div>
-            <button aria-label="burger" type="button" className={`burger-button ${toggleMenu ? "burger-button_type_open" : ""}`} onClick={toggleNav}></button>
-        </header>
+            <header className="header">
+                <img src={logo} alt="Logo representing the Around the US project" className="logo" />
+                <div className={`header__wrapper ${loggedIn ? "header__wrapper_type_login" : ""}`}>
+                    <p className="header__email">{loggedIn && userEmail}</p>
+                    <Link to={pathDefinition()} className={`header__link ${loggedIn ? "header__link_type_logout" : ""}`} onClick={loggedIn && handleLogout}>{linkText}</Link>
+                </div>
+                {loggedIn && <button aria-label="burger" type="button" className={`burger-button ${toggleMenu ? "burger-button_type_open" : ""}`} onClick={toggleNav}></button>}
+            </header>
+        </>
     )
 }
 
